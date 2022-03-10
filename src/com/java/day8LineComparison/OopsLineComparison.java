ed1ae7a019f1ff69020e04c59cfa0bc8c49db707
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-10 13:54:26 +0530

    Adding the Welcome message.

---
+++ src/com/java/day8LineComparison/OopsLineComparison.java
@@ -0,0 +1,15 @@
+package com.java.day8LineComparison;
+
+import java.util.Scanner;
+
+public class OopsLineComparison {
+    public static Scanner scanner =new Scanner(System.in);
+    public static double x1,y1;
+    public static double x2,y2;
+    public static double x3,y3;
+    public static double x4,y4;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Program on Master Branch");
+    }
+}
